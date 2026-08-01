59ace123f1f409e6f7c7db95173873d39ed15a03
Kaviraj Thangapandian <kaviraj.thangapandian@gmail.com>
2026-08-02 02:17:19 +0530

    Strip all invalid Lenis options

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -5,13 +5,6 @@ export const useLenis = () => {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
     });
 
     function raf(time: number) {
